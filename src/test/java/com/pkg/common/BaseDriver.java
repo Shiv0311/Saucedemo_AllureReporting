dad4099c246bc64e5e07e0b2c5c370ee70d21dc6
Shivkumar Konnuri <shivkumarkonnuri@gmail.com>
2024-10-30 15:53:03 +0530

    Updated Base class

--- src/test/java/com/pkg/common/BaseDriver.java
+++ src/test/java/com/pkg/common/BaseDriver.java
@@ -1,56 +1,40 @@
 package com.pkg.common;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
 
-public abstract class BaseDriver {
-    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    private final String hubURL = "http://localhost:4444/wd/hub"; // Selenium Grid Hub URL
 
-    @BeforeClass
+public class BaseDriver {
+    private WebDriver driver;
+
+    @BeforeSuite
     @Parameters({"browser"})
     public void setUp(String browser) throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(browser);
 
         // Set the capabilities for each browser
         if (browser.equalsIgnoreCase("chrome")) {
-            capabilities.setCapability("goog:chromeOptions", new HashMap<String, Object>() {{
-                put("args", Arrays.asList("--start-maximized"));
-            }});
+            driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("firefox")) {
-            capabilities.setCapability("moz:firefoxOptions", new HashMap<String, Object>() {{
-                put("args", Arrays.asList("--start-maximized"));
-            }});
+            driver = new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("edge")) {
-            capabilities.setCapability("ms:edgeOptions", new HashMap<String, Object>() {{
-                put("args", Arrays.asList("--start-maximized"));
-            }});
+            driver = new EdgeDriver();
         }
 
-        // Create and return the RemoteWebDriver instance
-        driver.set(new RemoteWebDriver(new URL(hubURL), capabilities));
     }
 
-    public WebDriver getDriver() {
-        return driver.get();
-    }
 
-    @AfterClass(alwaysRun = true)
+    @AfterSuite
     public void tearDown() {
         // Close the browser
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove(); // Clean up the ThreadLocal
+        if (driver != null) {
+            driver.quit();// Clean up the ThreadLocal
         }
     }
 }
